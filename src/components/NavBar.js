47cdfcbdd632e574f9a92e4addc9d8226852ab99
Ignacio Rodriguez Justo <38332696+Nachrj@users.noreply.github.com>
2020-01-17 23:39:06 -0300

    Finished NavBar
    Added Montserrat font and a little animation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,18 +6,20 @@ import {
     Link
   } from "react-router-dom";
 
+import { useRef, useState, useEffect } from 'react';
 import StartingPage from './StartingPage.js'
 import AboutPage from './AboutPage.js'
 import ProjectPage from './ProjectPage.js'
 import ContactPage from './ContactPage.js'
 import '../style/NavBar.css'
 
-const LinkStyle = {
-    textDecoration: 'none',
-    color: 'white',
-}
-
 const NavBar = () => {
+
+    const [hoverRef, isHovered] = useHover();
+    const [hoverRef1, isHovered1] = useHover();
+    const [hoverRef2, isHovered2] = useHover();
+    const [hoverRef3, isHovered3] = useHover();
+
     return(
         // Use React Router to move between screens. 
         // The 'linking' happens inside the Switch component
@@ -25,10 +27,11 @@ const NavBar = () => {
         // the first screen.
         <Router>
             <nav className="navbar">
-                <Link to="/home" style={LinkStyle}>Home</Link>
-                <Link to="/about" style={LinkStyle}>About</Link>
-                <Link to="/projects" style={LinkStyle}>Projects</Link>
-                <Link to="/contact" style={LinkStyle}>Contact</Link>
+            {/* This changes de link's class name depending on if its hovered or not */}
+                <Link to="/home" ref={hoverRef} className={ isHovered ? 'linkStyleHover' : 'linkStyle' }>Home</Link>
+                <Link to="/about" ref={hoverRef1} className={ isHovered1 ? 'linkStyleHover' : 'linkStyle' }>About</Link>
+                <Link to="/projects" ref={hoverRef2} className={ isHovered2 ? 'linkStyleHover' : 'linkStyle' }>Projects</Link>
+                <Link to="/contact" ref={hoverRef3} className={ isHovered3 ? 'linkStyleHover' : 'linkStyle' }>Contact</Link>
             </nav>
 
             <Switch>
@@ -48,5 +51,32 @@ const NavBar = () => {
         </Router>
     )
 }
+
+// Hook
+function useHover() {
+    const [value, setValue] = useState(false);
+  
+    const ref = useRef(null);
+  
+    const handleMouseOver = () => setValue(true);
+    const handleMouseOut = () => setValue(false);
+  
+    useEffect(
+      () => {
+        const node = ref.current;
+        if (node) {
+          node.addEventListener('mouseover', handleMouseOver);
+          node.addEventListener('mouseout', handleMouseOut);
+  
+          return () => {
+            node.removeEventListener('mouseover', handleMouseOver);
+            node.removeEventListener('mouseout', handleMouseOut);
+          };
+        }
+      },
+    );
+  
+    return [ref, value];
+  }
 
 export default NavBar
